a420513fabb8ebf8778f451fe2c23041a7de9e67
“neyser-eviden” <neyser.huaman.external@eviden.com>
2024-04-22 20:39:58 +0200

    Uso de tuplas y tipos de datos personalizados

---
+++ Ejemplo6.ts
@@ -0,0 +1,14 @@
+// Uso de tuplas y Tipos de Datos
+type datosSilla= [string, string, number]
+let sillas1: ["Oficina", "verde", 4]
+let sillas2: datosSilla=["Oficina", "rojo", 5]
+let sillas3: datosSilla=["casa", "azul", 7]
+let sillas4: datosSilla=["playa", "verde", 2]
+
+sillas2[2]=8
+sillas3[0]="Baño"
+
+console.log(sillas2)
+console.log(sillas3)
+
+
